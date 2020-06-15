2dcfaa2aa3cd56e928c1b144d6ca2d2483597972
Willem Meijer <willem1998m@hotmail.com>
2020-06-15 22:03:04 +0200

    Back up can be interrupted

--- SimpleBackUpTool/SimpleBackUpTool/src/BackUpProgram.cs
+++ SimpleBackUpTool/SimpleBackUpTool/src/BackUpProgram.cs
@@ -15,6 +15,7 @@ namespace SimpleBackUpTool
         private BackUp backUp;
         private NotifyIcon trayIcon;
         private Thread loadIconThread;
+        private Thread backUpThread;
 
         public static string AppDataRoot
         {
@@ -71,6 +72,19 @@ namespace SimpleBackUpTool
         }
 
 
+        private void LoadBackingUpMenu()
+        {
+            ContextMenu contextMenu = new ContextMenu(
+                new MenuItem[]
+                {
+                    new MenuItem("Exit", (sender, e) => {  StopBackUp(); Exit(sender, e); }),
+                    new MenuItem("Stop Back-Up", (sender, e) => { StopBackUp(); Reloadmenu(); })
+                });
+
+            trayIcon.ContextMenu = contextMenu;
+        }
+
+
         private void Exit(object sender, EventArgs e)
         {
             // Hide tray icon, otherwise it will remain shown until user mouses over it
@@ -80,40 +94,54 @@ namespace SimpleBackUpTool
 
         private void StartBackUp(string userSettings)
         {
-            LoggingUtilities.SetAppDataRoot(AppDataRoot);
-            try
-            {
-                LoadingIcon();
-                LoggingUtilities.LogFormat("\nInitiating Simple Back-Up ({0})\n", DateTime.Now.ToString());
-                
-                BackUpSettings loadedSettings = settings.Load(userSettings);
+            LoadBackingUpMenu();
 
-                backUp = new BackUp();
-                backUp.Start(loadedSettings);
-            }
-            catch (Exception ex)
+            backUpThread = new Thread(() =>
             {
-                LoggingUtilities.Log("ERROR\n");
-                LoggingUtilities.Log(ex.StackTrace + "\n");
-                LoggingUtilities.Log(ex.Message + "\n");
-                LoggingUtilities.Log(ex.Data.ToString() + "\n");
-
-                Console.Beep(200, 500);
-                Console.Beep(200, 500);
-                Console.Beep(200, 500);
-            }
-            finally
-            {
-                if(loadIconThread != null)
+                LoggingUtilities.SetAppDataRoot(AppDataRoot);
+                try
                 {
-                    loadIconThread.Abort();
-                    loadIconThread = null;
-                    trayIcon.Icon = Resources.AppIcon;
-                }
-            }
+                    LoadingIcon();
+                    LoggingUtilities.LogFormat("\nInitiating Simple Back-Up ({0})\n", DateTime.Now.ToString());
+                
+                    BackUpSettings loadedSettings = settings.Load(userSettings);
 
-            Console.Beep(400, 500);
-            LoggingUtilities.LogFormat("Finished AFB ({0})\n", DateTime.Now.ToString());
+                    backUp = new BackUp();
+                    backUp.Start(loadedSettings);
+                }
+                catch (Exception ex)
+                {
+                    LoggingUtilities.Log("ERROR\n");
+                    LoggingUtilities.Log(ex.StackTrace + "\n");
+                    LoggingUtilities.Log(ex.Message + "\n");
+                    LoggingUtilities.Log(ex.Data.ToString() + "\n");
+
+                    Console.Beep(200, 500);
+                    Console.Beep(200, 500);
+                    Console.Beep(200, 500);
+                }
+                finally
+                {
+                    if(loadIconThread != null)
+                    {
+                        loadIconThread.Abort();
+                        loadIconThread = null;
+                        trayIcon.Icon = Resources.AppIcon;
+                    }
+                }
+
+                Console.Beep(400, 500);
+                LoggingUtilities.LogFormat("Finished AFB ({0})\n", DateTime.Now.ToString());
+            });
+
+            backUpThread.Start();
+        }
+
+        private void StopBackUp()
+        {
+            backUpThread.Abort();
+            loadIconThread.Abort();
+            trayIcon.Icon = Resources.AppIcon;
         }
 
         private void CreateNewBackUpSettings(object sender, EventArgs e)
